057bf1a470a1bc48cecf7edca7363a7e284f3e8f
BertA-immalle <bert.anthonissen@immalle.be>
2017-05-09 16:09:00 +0200

    inlog project

---
+++ resources/views/user/comments.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!DOCTYPE html>
+<style>
+    .h1{
+        display: flex;
+        justify-content: center;
+        aling-content: center;
+    }
+</style>
+
+<html>
+    <body>
+
+        <h1>hello world</h1>
+
+    </body>
+</html>
+
+@endsection
